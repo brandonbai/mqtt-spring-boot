c18ef35befa53ab06f31ae44de58bc888706184e
Brandon <jiflytiger@163.com>
2019-05-19 10:28:13 +0800

    add spring boot sample

---
+++ samples/mqtt-spring-boot-sample/src/test/java/io/github/brandonbai/sample/mqtt/MqttSpringBootSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.brandonbai.sample.mqtt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MqttSpringBootSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
